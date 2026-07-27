88a2821359744688511c26fba1dc6ba003d727c0
yingdemu <2458319912@qq.com>
2026-07-27 20:39:33 +0800

    备份

--- SeekFree_MM32F327X_G8P_Opensource_Library/project/code/control.c
+++ SeekFree_MM32F327X_G8P_Opensource_Library/project/code/control.c
@@ -20,7 +20,7 @@
 #include "control.h"
 
 // 阿克曼差速全局变量
-float ackermann_gain = 1.00f;                                                   // 差速增益（蓝牙可调）
+float ackermann_gain = 3.00f;                                                   // 差速增益（蓝牙可调）
 
 //==================================================== 控制模块初始化 ====================================================
 
@@ -195,6 +195,6 @@ void ackermann_differential(float servo_angle_deg, float base_duty, float *left_
 
     // 正角（左转）：左轮减速、右轮加速
     // 负角（右转）：左轮加速、右轮减速（tan负值自动反转）
-    *left_duty  = base_duty * (1.0f + diff);
-    *right_duty = base_duty * (1.0f - diff);
+    *left_duty  = base_duty * (1.0f - diff);
+    *right_duty = base_duty * (1.0f + diff);
 }
--- SeekFree_MM32F327X_G8P_Opensource_Library/project/user/src/main.c
+++ SeekFree_MM32F327X_G8P_Opensource_Library/project/user/src/main.c
@@ -236,11 +236,10 @@ menu_need_refresh = 1;                                                        //
                     float groy_z = get_gyro_z();
                     float IMU_target = image_pid_set(0, IMG_W/2 - weight_position);
                     float servo_angle = IMU_pid_set(IMU_target, groy_z);
-                    float dif_motor = motor_pid_set(0, IMG_W/2 - weight_position);
-                    float actual_motor_duty=motor_duty-abs(image_pid_error*turn_rate);
+                    //float actual_motor_duty=motor_duty-abs(image_pid_error*turn_rate);
                     servo_set_angle(servo_angle);
 
-                    ackermann_differential( servo_angle,  actual_motor_duty, &left_duty, &right_duty);
+                    ackermann_differential( servo_angle,  motor_duty, &left_duty, &right_duty);
 
                     motor_set_duty(left_duty, right_duty);  
                 }
